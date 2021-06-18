82aa6618e4ca5dbd4b6eb428461f98af0e672052
Isaac <isaac.ewing3@gmail.com>
2021-06-17 18:02:54 -0600

    tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -98,5 +98,83 @@ describe('app routes', () => {
 
       expect(data.body).toEqual(expectation);
     });
+
+    test('make a new game', async() => {
+
+      const expectation = 
+      {
+        id: 'Spelunky',
+        avgPlayers: 30000,
+        fun: true,
+        type: 'rouge',
+        owner_id: 1
+      }; 
+      
+      const data = await fakeRequest(app)
+        .post('/games')
+        .send({
+          id: 'Spelunky',
+          avgPlayers: 30000,
+          fun: true,
+          type: 'rouge',
+        })
+        .expect('Content-Type', /json/);
+        
+      const gamesData = await fakeRequest(app) 
+        .get('/games')
+        .expect('Content-Type', /json/)
+        .expect(200); 
+
+      expect(data.body).toEqual(expectation);
+      expect(gamesData.body).toContainEqual(expectation);
+    });
+
+    test('/PUT board-games updates a single board game', async() => {
+      const data = await fakeRequest(app)
+        .put('/games/Destiny')
+        .send({
+          id: 'Destiny2',
+          avgPlayers: 20000,
+          fun: true,
+          type: 'none'
+        })
+        .expect('Content-Type', /json/);
+      const dataGames = await fakeRequest(app)
+        .get('/games')
+        .expect('Content-Type', /json/)
+        .expect(200);
+  
+      const newGame = { 
+        'id': 'Destiny2',
+        'avgPlayers': 20000,
+        'fun': true,
+        'type': 'none'
+      };
+      expect(data.body).toEqual(newGame);
+      expect(dataGames.body).toContainEqual(newGame);
+    });
+
+    test('test delete', async() => {
+
+      const expectation = 
+      {
+        id: 'Spelunky',
+        avgPlayers: 30000,
+        fun: true,
+        type: 'rouge',
+      };
+      
+      await fakeRequest(app)
+        .delete('/games/Spelunky')
+        .expect('Content-Type', /json/)
+        .expect(200);
+      
+      const allGames = await fakeRequest(app) 
+        .get('/games')
+        .expect('Content-Type', /json/)
+        .expect(200); 
+     
+      expect(allGames.body).not.toContainEqual(expectation);
+    });
   });
 });
